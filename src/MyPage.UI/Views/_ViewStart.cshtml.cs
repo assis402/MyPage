8e905595a2f37c1826141306c4405ae2d8605b37
assis402 <assis4002@gmail.com>
2023-02-18 15:16:44 -0300

    Adicionar arquivos de projeto.

---
+++ src/MyPage.UI/Views/_ViewStart.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyPage.UI.Views
+{
+    public class _ViewStartModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
