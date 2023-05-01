41447821aa26d48b3154e22113f22d2f6689241c
Matheus de Assis <assis4002@gmail.com>
2023-05-01 13:48:21 -0300

    test staticfiles

--- src/MyPage.UI/Program.cs
+++ src/MyPage.UI/Program.cs
@@ -52,6 +52,11 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseStaticFiles("/matheus/mypage");
+}
+else
+{
+    app.UseStaticFiles();
 }
 
 var supportedCultures = new[]
@@ -69,7 +74,6 @@ app.UseRequestLocalization(new RequestLocalizationOptions
 
 app.UseForwardedHeaders();
 app.UseHttpsRedirection();
-app.UseStaticFiles();
 
 app.UseRouting();
 
